06e9429bc118530b6503a8a63e37ac92c6a37f86
daebangstn <gunho1107@naver.com>
2024-07-16 11:29:27 +0900

    interaction line

--- examples/load_INTERGEN.py
+++ examples/load_INTERGEN.py
@@ -5,6 +5,8 @@ import numpy as np
 from aitviewer.configuration import CONFIG as C
 from aitviewer.renderables.smpl import SMPLSequence
 from aitviewer.viewer import Viewer
+from aitviewer.renderables.lines import Lines
+
 
 if __name__ == "__main__":
     # Load an AMASS sequence and make sure it's sampled at 60 fps. This automatically loads the SMPL-H model.
@@ -18,9 +20,19 @@ if __name__ == "__main__":
         show_joint_angles=True,
     )
 
+    line_shape = list(seq1.joints.shape)
+    line_shape[1] *= 2
+    line_pos = np.zeros(line_shape)
+    line_pos[:, ::2, :] = seq1.joints
+    line_pos[:, 1::2, :] = seq2.joints
+    line_pos_z_up = line_pos[:, :, [0, 2, 1]]
+    c = (0 / 255, 85 / 255, 200 / 255, 0.5)
+    line_renderable = Lines(line_pos, color=c, r_base=0.004)
+
     # Display in the viewer.
     v = Viewer()
     v.run_animations = True
-    v.scene.camera.position = np.array([2.8, 2.3, -4.5])
+    v.scene.camera.position = np.array([1.5, 2, 2.5])
     v.scene.add(seq1, seq2)
+    v.scene.add(line_renderable)
     v.run()
